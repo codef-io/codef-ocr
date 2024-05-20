35eeadbfa3dfbd46d161b7629af45ae86fe49879
TaesuSon <xotn0929@hectodata.co.kr>
2024-05-20 10:46:52 +0900

    OCR WebRTC Sample Soucre Create

---
+++ src/main/java/io/codef/ocr/controller/ApiSampleController.java
@@ -0,0 +1,40 @@
+package io.codef.ocr.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.codef.ocr.service.ApiSampleService;
+
+
+
+
+@Controller
+public class ApiSampleController {
+	
+	@Autowired
+	private ApiSampleService sampleService;
+	
+	@PostMapping("/ocrtest")
+	@ResponseBody
+	public String ocrAPI(HttpServletRequest request, HttpServletResponse response, @RequestBody String reqdata) throws InterruptedException, IOException, ParseException {
+		
+		String returnData = "";
+    	JSONParser parser = new JSONParser();
+    	JSONObject jsonObject = (JSONObject) parser.parse(reqdata);
+    	
+        returnData = sampleService.ocrAPI(jsonObject.get("base64Data").toString(), jsonObject.get("ocrType").toString());
+        
+		return returnData;
+	}
+	
+}
